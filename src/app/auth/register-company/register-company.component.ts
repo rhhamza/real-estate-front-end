bf63d31a0b903afb55d02bc0fd65c9e5628a70cb
Ramik9 <ramikaabi93@gmail.com>
2023-07-08 00:39:52 +0100

    wip

--- src/app/auth/register-company/register-company.component.ts
+++ src/app/auth/register-company/register-company.component.ts
@@ -1,4 +1,10 @@
+import { Roles } from 'src/app/core/models/roles.model';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NotifierService } from 'angular-notifier';
+import { IUserEntity } from 'src/app/core/interfaces/user.interface';
+import { UserService } from 'src/app/core/services/user.service';
+import { StatusType, UserEntity } from 'src/app/core/models/user-entity.model';
 
 @Component({
   selector: 'app-register-company',
@@ -7,9 +13,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterCompanyComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup = new FormGroup({});
+  
+  submitted = false;
 
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    public userService: UserService,
+    //private modalService: NgbModal,
+    private notifier: NotifierService
+  ) { 
+    this.registerForm = this.formBuilder.group({
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      
+  });
   }
+
+  ngOnInit() {    
+    console.log("Register form initialized  ");
+    }
+  
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {  
+    this.submitted = true;
+    if (this.registerForm.invalid) {      
+        return;
+    }
+  
+  const user1: UserEntity = {
+    firstname: this.registerForm.get('first_name')?.value,
+    lastname: this.registerForm.get('last_name')?.value,
+    email: this.registerForm.get('email')?.value,
+    password: this.registerForm.get('password')?.value,
+    status: StatusType.INACTIVE,
+    roles: [{ ID: 1, name: 'company' }],
+    publications: [],
+    comments: [],
+    reacts: [],
+    reports: []
+  };  
+  this.userService.registerCompany(user1).subscribe(() => {  // 200
+    this.submitted = false;
+    this.registerForm.reset()
+    this.notifier.notify(
+      'success',
+      'Registered User with success'
+    );
+  }, err => { // 400 500 
+    this.notifier.notify(
+      'error',
+      'User Exists'
+    );
+  })
+}
+
+
 
 }
